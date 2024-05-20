8d7c993cf91b2ad9b4fe97ec36ed9f3175983967
oumaimabrahmi <brahmioumaima9@gmail.com>
2024-05-20 18:32:52 +0100

    users:delete-unverified in 1 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('users:delete-unverified')->cron('*/2 * * * *');
+        $schedule->command('users:delete-unverified')->cron('*/1 * * * *');
     }
 
     /**
