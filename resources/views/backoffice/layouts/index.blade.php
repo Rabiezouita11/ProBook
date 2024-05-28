ca3b802834adbd902130363dac7e83f5d7794067
oumaimabrahmi <brahmioumaima9@gmail.com>
2024-05-28 21:08:04 +0100

    remouve icon notification et message :layout admin

--- resources/views/backoffice/layouts/index.blade.php
+++ resources/views/backoffice/layouts/index.blade.php
@@ -118,29 +118,7 @@
 								</svg></i>
 						</a>
 					</li>
-					<li>
-						<a class="mesg-notif" href="#" title="Messages" data-toggle="tooltip">
-							<i>
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-									fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-									stroke-linejoin="round" class="feather feather-message-square">
-									<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-								</svg></i>
-						</a>
-						<span></span>
-					</li>
-					<li>
-						<a class="mesg-notif" href="#" title="Notifications" data-toggle="tooltip">
-							<i>
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-									fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-									stroke-linejoin="round" class="feather feather-bell">
-									<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-									<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-								</svg></i>
-						</a>
-						<span></span>
-					</li>
+
 					
 					<li>
 						<a title="" href="#">
@@ -294,102 +272,7 @@
 			</div>
 		</div><!-- send message popup -->
 
-		<div class="side-slide">
-			<span class="popup-closed"><i class="icofont-close"></i></span>
-			<div class="slide-meta">
-				<ul class="nav nav-tabs slide-btns">
-					<li class="nav-item"><a class="active" href="#messages" data-toggle="tab">Messages</a></li>
-					<li class="nav-item"><a class="" href="#notifications" data-toggle="tab">Notifications</a></li>
-				</ul>
-				<!-- Tab panes -->
-				<div class="tab-content">
-					<div class="tab-pane active fade show" id="messages">
-						<h4><i class="icofont-envelope"></i> messages</h4>
-						<a href="#" class="send-mesg" title="New Message" data-toggle="tooltip"><i
-								class="icofont-edit"></i></a>
-						<ul class="new-messages">
-							<li>
-								<figure><img src="/backoffice/images/resources/user1.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Ibrahim Ahmed</span>
-									<a href="#" title="">Helo dear i wanna talk to you</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user2.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Fatima J.</span>
-									<a href="#" title="">Helo dear i wanna talk to you</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user3.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Fawad Ahmed</span>
-									<a href="#" title="">Helo dear i wanna talk to you</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user4.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Saim Turan</span>
-									<a href="#" title="">Helo dear i wanna talk to you</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user5.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">Helo dear i wanna talk to you</a>
-								</div>
-							</li>
-						</ul>
-						<a href="#" title="" class="main-btn" data-ripple="">view all</a>
-					</div>
-					<div class="tab-pane fade" id="notifications">
-						<h4><i class="icofont-bell-alt"></i> notifications</h4>
-						<ul class="notificationz">
-							<li>
-								<figure><img src="/backoffice/images/resources/user5.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">recommend your post</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user4.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">share your post <strong>a good time today!</strong></a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user2.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">recommend your post</a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user1.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">share your post <strong>a good time today!</strong></a>
-								</div>
-							</li>
-							<li>
-								<figure><img src="/backoffice/images/resources/user3.jpg" alt=""></figure>
-								<div class="mesg-info">
-									<span>Alis wells</span>
-									<a href="#" title="">recommend your post</a>
-								</div>
-							</li>
-						</ul>
-						<a href="#" title="" class="main-btn" data-ripple="">view all</a>
-					</div>
-				</div>
-			</div>
-		</div><!-- side slide message & popup -->
+		
 	</div>
 
 	<script src="/backoffice/js/main.min.js"></script>
